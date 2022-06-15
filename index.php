7dd59a8a32f15ac587bb9a342e1f1bdcee933763
lim44104 <s10207455@connect.np.edu.sg>
2022-06-15 14:52:13 +0800

    Add index file, stylesheet and script

---
+++ index.php
@@ -0,0 +1,85 @@
+<html>
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Mock Up Assessment</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    </head>
+    <body class="bg">
+
+    <div class="row">
+        <div class="card text-white mb-3 banner" style="background-color: grey;">
+            <div class="card-body">
+                <div class="container">
+                <img src = "images/exclamation-triangle.svg" alt="Warning" style="width: 30px; float: left;" class="filter-white"/>
+                    <h3 class="card-title" style="text-align: left; margin-top: 20px; font-weight: bold;">&nbsp Your browser Internet Explorer 11 is deprecated</h3>
+                    <p class="card-text hero-text">Modern browsers protect your device from viruses and support new technologies.<br>Thats why we decided to stop supporting your current browser or your browser version. To access our website,
+                        please download one of the browsers below.</p>
+                    <button type="button" class="btn btn-outline-light d-none d-sm-block d-sm-none d-md-block" style="margin-top: 10px; margin-bottom: 20px; border-radius: 0 !important; padding: 10px;">How do I update my browser?</button>
+                </div>             
+            </div>  
+        </div>
+    </div>
+        <div class="container">
+        <div class="row" style="margin-top: 40px;">
+            <div style="text-align: right;">
+                <div class="split left">
+                <img src = "images/chrome.svg" alt="Chrome" class="filter-white icon chrome-margin"/>
+                    <h5 style="margin-top: 20px; margin-right:30px; font-weight: bold;" class="browser-text">GOOGLE CHROME</h5>
+                    <h6 style="margin-top: 10px; margin-right:70px;" class="browser-text">Version 40+</h6>
+                    <img src = "images/download-svgrepo-com.svg" alt="Download" class="filter-white download-icon"/>  
+                    <div class="download"><button type="button" class="btn btn-outline-light download" style="margin-top:15px; margin-right:70px; border-radius: 0 !important;">Download</button></div>  
+                            
+                </div>           
+            </div>
+            <div class="col-lg-2" style="text-align: center;">
+                <div class="split2 middle-one">
+                    <img src = "images/firefox-browser.svg" alt="Firefox" class="filter-white icon"/>
+                    <h5 style="margin-top: 20px; font-weight: bold;" class="browser-text browser-text-middle">MOZILLA FIREFOX</h5>
+                    <h6 style="margin-top: 10px;" class="browser-text">Version 40+</h6>
+                    <img src = "images/download-svgrepo-com.svg" alt="Download" class="filter-white download-icon"/>  
+                    <div class="download"><button type="button" class="btn btn-outline-light" style="margin-top:15px; text-align:center; border-radius: 0 !important;">Download</button></div>  
+                </div>
+            </div>
+            <div class="col-lg-2" style="text-align: center;">
+                <div class="split2 middle-two">
+                    <img src = "images/edge.svg" alt="Edge" class="filter-white icon"/>
+                    <h5 style="margin-top: 20px; font-weight: bold;" class="browser-text browser-text-middle">MICROSOFT EDGE</h5>
+                    <h6 style="margin-top: 10px;" class="browser-text">Version 40+</h6>
+                    <img src = "images/download-svgrepo-com.svg" alt="Download" class="filter-white download-icon"/>  
+                    <div class="download"><button type="button" class="btn btn-outline-light" style="margin-top:15px; text-align:center; border-radius: 0 !important;">Download</button></div> 
+                </div>
+            </div>
+            <div class="col-lg-2" style="text-align: center;">
+                <div class="split2 middle-three">
+                    <img src = "images/safari.svg" alt="Safari" class="filter-white icon"/>
+                    <h5 style="margin-top: 20px; font-weight: bold;" class="browser-text browser-text-middle">APPLE SAFARI</h5>
+                    <h6 style="margin-top: 10px;" class="browser-text">Version 40+</h6>
+                    <img src = "images/download-svgrepo-com.svg" alt="Download" class="filter-white download-icon"/>  
+                    <div class="download"><button type="button" class="btn btn-outline-light" style="margin-top:15px; text-align:center; border-radius: 0 !important;">Download</button></div> 
+                </div>
+            </div>
+            <div class="col-lg-3" style="text-align: left;">
+                <div class="split right">
+                    <img src = "images/opera.svg" alt="Opera" class="filter-white icon opera-margin"/>
+                    <h5 style="margin-top: 20px; color: white; margin-left:80px; font-weight: bold;" class="browser-text">OPERA</h5>
+                    <h6 style="margin-top: 10px; color: white; margin-left:70px;" class="browser-text">Version 40+</h6>
+                    <img src = "images/download-svgrepo-com.svg" alt="Download" class="filter-white download-icon"/>  
+                    <div class="download"><button type="button" class="btn btn-outline-light" style="margin-top:15px; margin-left:70px; border-radius: 0 !important;">Download</button></div>
+                </div>
+            </div>
+        </div>
+        </div>
+
+            
+        <script src="js/script.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </body>
+</html>
